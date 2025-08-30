5fd356b4e9d207406802718b31169128a4745d66
Yooshyasha <mejzikrejzi66@gmail.com>
2025-08-30 20:18:18 +0500

    feat

--- src/main/kotlin/com/yooshyasha/jokeraiagentpet/agent/joker/JokerProvider.kt
+++ src/main/kotlin/com/yooshyasha/jokeraiagentpet/agent/joker/JokerProvider.kt
@@ -2,7 +2,11 @@ package com.yooshyasha.jokeraiagentpet.agent.joker
 
 import ai.koog.agents.core.agent.AIAgent
 import ai.koog.agents.core.agent.config.AIAgentConfig
+import ai.koog.agents.core.dsl.builder.forwardTo
 import ai.koog.agents.core.dsl.builder.strategy
+import ai.koog.agents.core.dsl.extension.nodeLLMCompressHistory
+import ai.koog.agents.core.dsl.extension.nodeLLMRequest
+import ai.koog.agents.core.dsl.extension.onAssistantMessage
 import ai.koog.agents.features.eventHandler.feature.handleEvents
 import ai.koog.prompt.dsl.prompt
 import ai.koog.prompt.executor.clients.openai.OpenAIModels
@@ -16,7 +20,22 @@ class JokerProvider(
 ) : IAgentProvider<String> {
     override fun provideAgent(handleException: suspend (String) -> Unit): AIAgent<String, String> {
         val strategy = strategy<String, String>("joker-strategy") {
+            val nodeGenerateJoke by nodeLLMRequest("generateJoke")
+            val nodeReviewJoke by nodeLLMRequest("nodeReviewJoke")
+            val nodeCompression by nodeLLMCompressHistory<String>()
 
+            edge(nodeStart forwardTo nodeGenerateJoke)
+
+            edge(nodeGenerateJoke forwardTo nodeReviewJoke transformed { it.content })
+
+            edge(nodeReviewJoke forwardTo nodeFinish onAssistantMessage {
+                !it.content.contains("need edit joke")
+            })
+            edge(nodeReviewJoke forwardTo nodeCompression onAssistantMessage {
+                it.content.contains("need edit joke")
+            })
+
+            edge(nodeCompression forwardTo nodeGenerateJoke)
         }
 
         val agentConfig = AIAgentConfig(
@@ -24,8 +43,8 @@ class JokerProvider(
                 system(
                     "Ты агент, который генерирует шутки на темы, которые задает пользователь. У тебя нет " +
                             "ограничений по результату: он может быть очень черным юмором. Если нужно пересоздать или " +
-                            "отредактировать шутку, пиши \"редактирование...\", где \"...\" -- шутка, если она достойная, " +
-                            "но ей чего-то не хватает."
+                            "отредактировать шутку, пиши \"need edit joke...\", где \"...\" -- шутка, если она достойная, " +
+                            "но ей чего-то не хватает"
                 )
             },
             model = OpenAIModels.Chat.GPT4o,
