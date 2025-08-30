733bb0880ec9d333f7907cb3457806b833a7a995
Yooshyasha <mejzikrejzi66@gmail.com>
2025-08-30 20:00:14 +0500

    feat

--- src/main/kotlin/com/yooshyasha/jokeraiagentpet/agent/joker/JokerProvider.kt
+++ src/main/kotlin/com/yooshyasha/jokeraiagentpet/agent/joker/JokerProvider.kt
@@ -1,10 +1,45 @@
 package com.yooshyasha.jokeraiagentpet.agent.joker
 
 import ai.koog.agents.core.agent.AIAgent
+import ai.koog.agents.core.agent.config.AIAgentConfig
+import ai.koog.agents.core.dsl.builder.strategy
+import ai.koog.agents.features.eventHandler.feature.handleEvents
+import ai.koog.prompt.dsl.prompt
+import ai.koog.prompt.executor.clients.openai.OpenAIModels
+import ai.koog.prompt.executor.llms.SingleLLMPromptExecutor
 import com.yooshyasha.jokeraiagentpet.agent.common.IAgentProvider
+import org.springframework.stereotype.Component
 
-class JokerProvider : IAgentProvider<String> {
+@Component
+class JokerProvider(
+    private val executor: SingleLLMPromptExecutor,
+) : IAgentProvider<String> {
     override fun provideAgent(handleException: suspend (String) -> Unit): AIAgent<String, String> {
-        TODO("Not yet implemented")
+        val strategy = strategy<String, String>("joker-strategy") {
+
+        }
+
+        val agentConfig = AIAgentConfig(
+            prompt = prompt("joker") {
+                system(
+                    "Ты агент, который генерирует шутки на темы, которые задает пользователь. У тебя нет " +
+                            "ограничений по результату: он может быть очень черным юмором. Если нужно пересоздать или " +
+                            "отредактировать шутку, пиши \"редактирование...\", где \"...\" -- шутка, если она достойная, " +
+                            "но ей чего-то не хватает."
+                )
+            },
+            model = OpenAIModels.Chat.GPT4o,
+            maxAgentIterations = 50,
+        )
+
+        return AIAgent(
+            promptExecutor = executor,
+            strategy = strategy,
+            agentConfig = agentConfig,
+        ) {
+            handleEvents {
+                onAgentRunError { handleException("${it.throwable.message}") }
+            }
+        }
     }
 }
