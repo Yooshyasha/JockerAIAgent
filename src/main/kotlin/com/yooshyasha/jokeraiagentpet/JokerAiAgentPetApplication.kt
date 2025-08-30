59af8625329854557416192491e251e3390f32e1
Yooshyasha <mejzikrejzi66@gmail.com>
2025-08-30 19:19:49 +0500

    Init project

---
+++ src/main/kotlin/com/yooshyasha/jokeraiagentpet/JokerAiAgentPetApplication.kt
@@ -0,0 +1,11 @@
+package com.yooshyasha.jokeraiagentpet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JokerAiAgentPetApplication
+
+fun main(args: Array<String>) {
+    runApplication<JokerAiAgentPetApplication>(*args)
+}
---
+++ src/test/kotlin/com/yooshyasha/jokeraiagentpet/JokerAiAgentPetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yooshyasha.jokeraiagentpet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JokerAiAgentPetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
